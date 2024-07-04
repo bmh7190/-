42c35ba9201c4931ca80050250ae2d82f9b1231e
gayo73 <163620291+gayo73@users.noreply.github.com>
2024-07-04 19:36:17 +0900

    Update views.py

--- hackathon/users/views.py
+++ hackathon/users/views.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import requests
 
 from django.shortcuts import render,redirect
@@ -16,31 +15,6 @@ from allauth.socialaccount.providers.oauth2.client import OAuth2Client
 from allauth.socialaccount.models import SocialAccount
 
 from .models import Profile,User
-
-=======
-
-from django.shortcuts import render,redirect
-from django.conf import settings
-
-from .models import Profile,User
->>>>>>> origin/backend_gayo
-from django.contrib.auth import authenticate
-from django.contrib.auth.models import update_last_login
-from django.shortcuts import get_object_or_404
-
-<<<<<<< HEAD
-from rest_framework.decorators import api_view, permission_classes,APIView
-=======
-from rest_framework import status
-from rest_framework.decorators import api_view, permission_classes, APIView
->>>>>>> origin/backend_gayo
-from rest_framework.permissions import AllowAny
-from rest_framework_simplejwt.tokens import RefreshToken
-
-from users.serializers import UserSerializer,ProfileSerializer
-from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
-
-
 
 
 
