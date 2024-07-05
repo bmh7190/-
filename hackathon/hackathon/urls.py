6af38a003ba597a29cff0cd158a05d19d5d97ca7
imh0ng <ghdgus0417@inha.edu>
2024-07-05 21:52:47 +0900

    add refresh

--- hackathon/hackathon/urls.py
+++ hackathon/hackathon/urls.py
@@ -2,6 +2,10 @@ from django.contrib import admin
 from django.urls import include, path
 from django.conf import settings
 from django.conf.urls.static import static
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+)
 
 
 urlpatterns = [
@@ -10,4 +14,6 @@ urlpatterns = [
     path('users/',include('allauth.urls')),
     path('users/',include('users.urls')),
     path('health/', include('util.urls')),
+    path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 ]+ static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
