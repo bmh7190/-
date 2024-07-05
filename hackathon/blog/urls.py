5e64432ec299a3f9e78aeaa339627da157b97768
bmh7190 <bmh7190@naver.com>
2024-07-05 20:32:37 +0900

    주소 수정

--- hackathon/blog/urls.py
+++ hackathon/blog/urls.py
@@ -5,10 +5,10 @@ urlpatterns = [
     path('tags/', TagList.as_view(), name='tag-list'),
     path('tags/<int:pk>/', TagDetail.as_view(), name='tag-detail'),
 
-    path('posts/<int:post_pk>', PostList.as_view(), name='post-list'),
+    path('posts/', PostList.as_view(), name='post-list'),
     path('posts/<int:pk>/', PostDetail.as_view(), name='post-detail'),
 
-    path('comments/', CommentList.as_view(), name='comment-list'),
+    path('comments/<int:post_pk>', CommentList.as_view(), name='comment-list'),
     path('comments/<int:pk>/', CommentDetail.as_view(), name='comment-detail'),
 
     path('bookmarks/', BookmarkList.as_view(), name='bookmark-list'),
